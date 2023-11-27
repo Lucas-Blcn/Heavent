9f88b467dda0aea82af8b3c9cc82c594787b4b92
lucas bilancini <lucasbilancini@gmail.com>
2023-11-27 18:06:16 +0100

    adding validation rules and seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-   # 1:N
-   has_many :interests
-   has_many :events, through: :interests
-   has_many :reviews, through: :interests
+  # 1:N
+  has_many :interests
+  has_many :events, through: :interests
+  has_many :reviews, through: :interests
 
-  # VALIDATION
-  # validates :status, presence: true
-  # validates :pseudo, presence: true, uniqueness: true
+   # VALIDATION
+   # validates :status, presence: true
+   # validates :pseudo, presence: true, uniqueness: true
+
+  #  validates :status, inclusion: { in: ['étudiant', 'CDI', 'CDD', ''] }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
