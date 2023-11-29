e30434d6bf198b54b44c08bde1c73014e8621de5
Pierre Lam <hoanlam@orange.fr>
2023-11-29 14:42:04 +0100

    Setup map + debut de view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_161313) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_115820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_161313) do
     t.string "background_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "interests", force: :cascade do |t|
