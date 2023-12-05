010a5a6461c674482c8fc43d13d2c0635eba58ea
Pierre Lam <hoanlam@orange.fr>
2023-12-05 22:05:28 +0100

    Ajout du bouton pour l'expansion de la description et amélioration de l'animation

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,15 +2,26 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  static targets = ["card", "modal", "close"];
+  static targets = ["card", "modal", "close", "modalContent"];
 
   connect() {}
 
   show() {
     this.modalTarget.style.display = "block";
+    // this.modalTarget.classList.add("show-modal")
+    setTimeout(() => {
+      this.modalContentTarget.classList.add("show-modal-content");
+    }, 10);
   }
 
   close() {
-    this.modalTarget.style.display = "none";
+    setTimeout(() => {
+      this.modalContentTarget.classList.remove("show-modal-content");
+      this.modalTarget.classList.remove("show-modal");
+    }, 10);
+
+    setTimeout(() => {
+      this.modalTarget.style.display = "none";
+    }, 400);
   }
 }
