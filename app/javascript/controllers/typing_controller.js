cbcfa120cc5099d4803df8ddd0319e49ab14bc7d
lina moein <lina@moein.fr>
2023-12-07 12:32:29 +0100

    front show, modif titre index, suppression du texte dans la navbar

--- app/javascript/controllers/typing_controller.js
+++ app/javascript/controllers/typing_controller.js
@@ -1,24 +1,24 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {
-  static targets = ['text']
-  static values = { text: String }
+// export default class extends Controller {
+//   static targets = ['text']
+//   static values = { text: String }
 
-  connect() {
-    this.i = 0
-    this.speed = 50;
-    // var that = this;
-    // this.typing();
-    setTimeout(this.typing.bind(this), 2000);
+//   connect() {
+//     this.i = 0
+//     this.speed = 50;
+//     // var that = this;
+//     // this.typing();
+//     setTimeout(this.typing.bind(this), 2000);
 
-  };
+//   };
 
-  typing() {
-    if (this.i < this.textValue.length) {
-      this.textTarget.innerHTML += this.textValue.charAt(this.i);
-      this.i++;
+//   typing() {
+//     if (this.i < this.textValue.length) {
+//       this.textTarget.innerHTML += this.textValue.charAt(this.i);
+//       this.i++;
 
-      setTimeout(this.typing.bind(this), this.speed);
-    }
-  };
-}
+//       setTimeout(this.typing.bind(this), this.speed);
+//     }
+//   };
+// }
